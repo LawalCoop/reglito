30a47d9d8de7e6df1b5777ee863f3dc18293aed4
Jeronimo Clinaz <jeronimo.clinaz@lawal.coop>
2024-02-22 19:16:19 -0300

    first post

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use askama::Template;
 mod handlers;
-
+use serde::Deserialize;
 #[derive(Template)]
 #[template(path = "index.html")]
 struct HelloTemplate {}
 
 use axum::{
-    http::StatusCode,
+    http::{Response, StatusCode},
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, post},
     Router,
 };
+use axum::extract::Form;
 use tower_livereload::LiveReloadLayer;
+
+#[derive(Debug, Deserialize)]
+struct FormData {
+    name: String,
+    matricule: String,
+}
 
 async fn hello() -> impl IntoResponse {
     let template = HelloTemplate {};
@@ -21,14 +28,20 @@ async fn hello() -> impl IntoResponse {
             StatusCode::INTERNAL_SERVER_ERROR,
             format!("Failed to render template. Error: {err}"),
         )
-            .into_response(),
+        .into_response(),
     }
+}
+
+async fn initialize_rules_form(Form(data): Form<FormData>) -> impl IntoResponse {
+    let greeting = format!("Welcome, {}!", data.name);
+    Html(greeting).into_response()
 }
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(hello))
+        .route("/initialize", post(initialize_rules_form))
         .layer(LiveReloadLayer::new());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8005").await.unwrap();
