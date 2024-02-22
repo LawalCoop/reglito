04515e454f3b9200b41a67a4ab0520134d6bc0d2
joaquin mansilla <joaquin.mansilla@lawal.coop>
2024-02-22 17:53:58 -0300

    commit inicial

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct HelloTemplate {}
+
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+
+async fn hello() -> impl IntoResponse {
+    let template = HelloTemplate {};
+    match template.render() {
+        Ok(html) => Html(html).into_response(),
+        Err(err) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to render template. Error: {err}"),
+        )
+            .into_response(),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8005").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
