90d49ef21a098cba1cfaac7354e3a4f64775ca65
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2024-07-12 16:28:21 -0300

    adding credo

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Reglito.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:credo, "~> 1.7", runtime: false, only: :dev}
     ]
   end
 
