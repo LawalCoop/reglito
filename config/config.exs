6565a44a997bf5e8199baff2d65f77dd72b71e9d
joaquin mansilla <joaquin.mansilla@lawal.coop>
2024-08-27 17:25:48 -0300

    database disabled

--- config/config.exs
+++ config/config.exs
@@ -7,9 +7,9 @@
 # General application configuration
 import Config
 
-config :reglito,
-  ecto_repos: [Reglito.Repo],
-  generators: [timestamp_type: :utc_datetime]
+#config :reglito,
+  #ecto_repos: [Reglito.Repo],
+  #generators: [timestamp_type: :utc_datetime]
 
 # Configures the endpoint
 config :reglito, ReglitoWeb.Endpoint,
--- config/dev.exs
+++ config/dev.exs
@@ -1,14 +1,14 @@
 import Config
 
 # Configure your database
-config :reglito, Reglito.Repo,
-  username: "reglito",
-  password: "reglito",
-  hostname: "localhost",
-  database: "reglito_dev",
-  stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+# config :reglito, Reglito.Repo,
+#   username: "reglito",
+#   password: "reglito",
+#   hostname: "localhost",
+#   database: "reglito_dev",
+#   stacktrace: true,
+#   show_sensitive_data_on_connection_error: true,
+#   pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
