ffbca222c2f7f2c8fa47e989036f1d37ab7b492d
joaquin mansilla <joaquin.mansilla@lawal.coop>
2024-09-20 16:42:47 -0300

    cooperative form validations

--- lib/reglito_web/live/cooperative_data_live.ex
+++
@@ -1,2 +0,0 @@
-defmodule ReglitoWeb.CooperativeDataLive do
-end
--- lib/reglito_web/live/cooperative_live.ex
+++ lib/reglito_web/live/cooperative_live.ex
@@ -1,61 +1,86 @@
 defmodule ReglitoWeb.CooperativeLive do
   use ReglitoWeb, :live_view
 
-  # TODO: Setear la informacion de la cooperativa en la session
-
   def render(assigns) do
     ~H"""
     <div class="w-full flex flex-col items-center justify-center mt-40">
       <p class="font-bold text-xl mb-10">Completa con los datos de tu Cooperativa</p>
       <div class="w-full max-w-xs">
-        <form phx-submit="submit_coop" class="bg-white shadow-xl rounded px-8 pt-6 pb-8 mb-4">
-          <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="cooperative_name">
-              Nombre de la Cooperativa
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="cooperative_name"
-              name="cooperative_name"
-              type="text"
-              placeholder="Nombre de la Cooperativa"
-              phx-hook="Form"
-            />
-          </div>
-          <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="registration_number">
-              Matrícula
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="registration_number"
-              name="registration_number"
-              type="text"
-              placeholder="Matrícula"
-              phx-hook="Form"
-            />
-          </div>
-          <div class="flex items-center justify-center">
-            <button
-              type="submit"
-              class="flex justify-center ite bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-            >
-              Siguiente
+        <.simple_form for={@form} phx-change="validate" phx-submit="submit_coop">
+          <.input field={@form[:name]} label="Nombre de la Cooperativa" />
+          <.input field={@form[:registration_number]} label="Matricula" />
+          <:actions>
+            <button class="aflex justify-center ite bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+              Save
             </button>
-          </div>
-        </form>
+          </:actions>
+        </.simple_form>
       </div>
     </div>
     """
   end
 
-  def handle_event("submit_coop", %{"cooperative_name" => cooperative_name, "registration_number" => registration_number}, socket) do
-    redirect_path = "/api/register?cooperative_name=#{cooperative_name}&registration_number=#{registration_number}"
+  def mount(_, _session, socket) do
+    form =
+      to_form(%{"name" => "", "registration_number" => ""})
 
-    {:noreply, push_redirect(socket, to: redirect_path)}
+    socket =
+      socket
+      |> assign(:form, form)
+
+    {:ok, socket}
   end
 
-  def handle_event(_event, _params, socket) do
-    {:noreply, socket}
+  def handle_event(
+        "submit_coop",
+        %{"name" => name, "registration_number" => registration_number},
+        socket
+      ) do
+    errors = validate(name, registration_number)
+
+    if Enum.empty?(errors) do
+      redirect_path =
+        "/api/register?cooperative_name=#{name}&registration_number=#{registration_number}"
+
+      {:noreply, push_redirect(socket, to: redirect_path)}
+    else
+      form =
+        to_form(%{"name" => name, "registration_number" => registration_number},
+          errors: errors
+        )
+
+      {:noreply, assign(socket, :form, form)}
+    end
+  end
+
+  def handle_event(
+        "validate",
+        %{"name" => name, "registration_number" => registration_number},
+        socket
+      ) do
+    errors = validate(name, registration_number)
+
+    form =
+      to_form(%{"name" => name, "registration_number" => registration_number},
+        errors: errors
+      )
+
+    {:noreply, assign(socket, :form, form)}
+  end
+
+  defp validate(name, registration_number) do
+    case {String.length(name) <= 0, String.length(registration_number) <= 0} do
+      {true, true} ->
+        [name: {"Nombre Requerido", []}, registration_number: {"Matricula Requerida", []}]
+
+      {true, false} ->
+        [name: {"Nombre Requerido", []}]
+
+      {false, true} ->
+        [registration_number: {"Matricula Requerida", []}]
+
+      {false, false} ->
+        []
+    end
   end
 end
